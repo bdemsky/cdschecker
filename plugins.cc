d7d1b3708a3622c21f0841147b9cd7f9490dd87e
Peizhao Ou <peizhaoo@uci.edu>
2014-05-19 14:48:22 -0700

    add definition line

--- plugins.cc
+++ plugins.cc
@@ -6,12 +6,17 @@
 ModelVector<TraceAnalysis *> * registered_analysis;
 ModelVector<TraceAnalysis *> * installed_analysis;
 
+SCFence *wildcard_plugin;
+
 void register_plugins() {
 	registered_analysis=new ModelVector<TraceAnalysis *>();
 	installed_analysis=new ModelVector<TraceAnalysis *>();
 	registered_analysis->push_back(new SCAnalysis());
 	registered_analysis->push_back(new SPECAnalysis());
-	registered_analysis->push_back(new SCFence());
+
+	/** Initialize the wildcard_plugin */
+	wildcard_plugin = new SCFence();
+	registered_analysis->push_back(wildcard_plugin);
 }
 
 ModelVector<TraceAnalysis *> * getRegisteredTraceAnalysis() {
